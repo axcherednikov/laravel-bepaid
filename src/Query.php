603b739a23e3229af9879921e1a15de36d331fcb
Alexander <agel63@mail.ru>
2022-06-21 17:51:59 +0300

    Added FillingContract in method fill for query class

--- src/Query.php
+++ src/Query.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Excent\BePaidLaravel;
 
+use Excent\BePaidLaravel\Contracts\FillingDTOContract;
 use BeGateway\{QueryByPaymentToken, QueryByTrackingId, QueryByUid};
 use Excent\BePaidLaravel\Contracts\IGateway;
 use Excent\BePaidLaravel\Dtos\{QueryByPaymentTokenDto, QueryByTrackingIdDto, QueryByUidDto};
@@ -19,29 +20,13 @@ class Query extends GatewayAbstract
     ) {
     }
 
-    public function fill($data, $object = null): IGateway
+    public function fill(FillingDTOContract $data, $object = null): IGateway
     {
-        if (is_array($data)) {
-            if (array_key_exists('token', $data)) {
-                $this->operation = $this->queryByPaymentToken;
-            } elseif (array_key_exists('tracking_id', $data)) {
-                $this->operation = $this->queryByTrackingId;
-            } elseif (array_key_exists('uid', $data)) {
-                $this->operation = $this->queryByUid;
-            }
-        } else {
-            switch (get_class($data)) {
-                case QueryByPaymentTokenDto::class:
-                    $this->operation = $this->queryByPaymentToken;
-                    break;
-                case QueryByTrackingIdDto::class:
-                    $this->operation = $this->queryByTrackingId;
-                    break;
-                case QueryByUidDto::class:
-                    $this->operation = $this->queryByUid;
-                    break;
-            }
-        }
+        $this->operation = match (get_class($data)) {
+            QueryByPaymentTokenDto::class => $this->queryByPaymentToken,
+            QueryByTrackingIdDto::class => $this->queryByTrackingId,
+            QueryByUidDto::class => $this->queryByUid,
+        };
 
         return parent::fill($data, $object);
     }
