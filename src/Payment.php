c85c90f50e06a6ce5a21a250b3f33ef8502c7cae
Alexander <agel63@mail.ru>
2022-06-22 17:25:26 +0300

    Fix payment method

--- src/Payment.php
+++ src/Payment.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace Excent\BePaidLaravel;
 
-class Payment extends Authorization
+use BeGateway\PaymentOperation;
+use Excent\BePaidLaravel\Contracts\FillingDTOContract;
+use Excent\BePaidLaravel\Contracts\IGateway;
+use Excent\BePaidLaravel\Dtos\PaymentDto;
+use Illuminate\Support\Str;
+
+class Payment extends GatewayAbstract
 {
+    public function __construct(public PaymentOperation $operation)
+    {
+    }
+
+    public function fill(FillingDTOContract $data, $object = null): IGateway
+    {
+        if ($data instanceof PaymentDto && empty($data->tracking_id)) {
+            $data->tracking_id = Str::uuid()->toString();
+        }
+
+        return parent::fill($data, $object);
+    }
 }
